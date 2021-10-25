ae3ec4b9558b989249709fb2bbbb167720fb25f1
geremiassilva0093@outlook.com <geremiaspereira093>
2021-10-25 01:41:22 -0300

    Configuração de Hospedagem

--- include/conectar.php
+++ include/conectar.php
@@ -1,7 +1,15 @@
 <?php
-	$server='127.0.0.1:3308';
-	$user='root';
-	$password='';
-	$database='farmacia';
+	/*Configurações Banco Local*/
+	// $server='127.0.0.1:3308';
+	// $user='root';
+	// $password='';
+	// $database='farmacia';
+	// $connection = mysqli_connect($server, $user, $password, $database) or die ('Não possível conectar');
+	
+	/*Configurações Banco Hospedagem*/
+	$server='localhost';
+	$user='id17820698_gere93';
+	$password='xjtR4PpP>\S7}ps$';
+	$database='id17820698_farmaciasenac';
 	$connection = mysqli_connect($server, $user, $password, $database) or die ('Não possível conectar');
 ?>
